0139940bb2d841dd4be5cbac629490f540f49e29
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-05-24 11:11:47 +0200

    add: route tamagotchi.js

---
+++ routes/tamagotchi.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+//exporteert de route in het server.js mapje -> import indexRoute from ./routes/index.js
+
+const tamagotchiRoute = express.Router(); // difinieert de index.js route (staat in het mapje sever)
+
+tamagotchiRoute.get("/", (request, response) => {
+  response.render("tamagotchi");
+});
+
+//exporteert de route in het server.js mapje -> import indexRoute from ./routes/index.js
+
+export default tamagotchiRoute;
+//exporteert de route in het server.js mapje -> import indexRoute from ./routes/index.js
